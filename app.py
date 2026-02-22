4d17c98ef8dd2e2062ad5c8f13616e203b8864af
Ikaro Vieira <ikarovieiraa@gmail.com>
2026-02-22 20:39:48 -0300

    teste

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ with st.sidebar:
     st.markdown("**Core Developers**")
     st.markdown("🧠 *Enzo Ribeiro*")
     st.markdown("🧠 *Gabriel Siron*")
-    st.markdown("🧠 *Ikaro Vieira*")
+    st.markdown("🧠 *Ikaro*")
     
     st.write("")
 
